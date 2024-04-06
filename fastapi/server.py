3574ea06a6bd20b905ec715cffe4d5923db44238
chhaewxn <chaewon1019@ewhain.net>
2024-04-06 20:51:08 +0900

    fix: 디렉토리 경로 수정

--- fastapi/server.py
+++ fastapi/server.py
@@ -10,18 +10,18 @@ app = FastAPI()
 async def detect_objects(file: UploadFile = File(...)):
     
     # 이미지를 저장할 디렉토리 생성
-    os.makedirs('./fastapi/input_dir', exist_ok=True)
+    os.makedirs('./input_dir', exist_ok=True)
 
     # 이미지 파일의 경로 설정
     filename = secure_filename(file.filename)
-    file_path = os.path.join('./fastapi/input_dir', filename)
+    file_path = os.path.join('./input_dir', filename)
 
     # 이미지 파일 저장
     with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
 
     # YOLO 모델 초기화
-    model = YOLO('./fastapi/runs/detect/train/weights/best2.pt')
+    model = YOLO('./runs/detect/train/weights/best2.pt')
     results = model.predict(file_path, save=True, imgsz=640, conf=0.3)
 
     # 객체 인식 결과를 리스트로 저장
