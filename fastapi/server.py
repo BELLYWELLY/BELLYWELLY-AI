cf0247289d18f08f2e564652d34bbf8c535bf56d
chhaewxn <chaewon1019@ewhain.net>
2024-04-08 01:34:11 +0900

    feat: 가중치 파일 추가

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,33 +1,43 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, HTTPException, UploadFile, File, Body, Request 
 from fastapi.responses import JSONResponse
 from ultralytics import YOLO
 from werkzeug.utils import secure_filename
 import os
+import requests
 
 app = FastAPI()
 
 @app.post("/detection")
-async def detect_objects(file: UploadFile = File(...)):
-    
-    # 이미지를 저장할 디렉토리 생성
-    os.makedirs('./input_dir', exist_ok=True)
+async def detect_objects(request: Request):
+    try:
+        # 요청 바디 파싱
+        body = await request.json()
+        image_url = body.get("image_url")
 
-    # 이미지 파일의 경로 설정
-    filename = secure_filename(file.filename)
-    file_path = os.path.join('./input_dir', filename)
+        # 이미지를 다운로드하여 저장할 디렉토리 생성
+        os.makedirs('./input_dir', exist_ok=True)
 
-    # 이미지 파일 저장
-    with open(file_path, "wb") as buffer:
-        buffer.write(await file.read())
+        # 이미지 파일의 이름 설정
+        filename = secure_filename("input_image.jpg")
+        file_path = os.path.join('./input_dir', filename)
 
-    # YOLO 모델 초기화
-    model = YOLO('./runs/detect/train/weights/best2.pt')
-    results = model.predict(file_path, save=True, imgsz=640, conf=0.3)
+        # 이미지 다운로드
+        response = requests.get(image_url)
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
 
-    # 객체 인식 결과를 리스트로 저장
-    output_labels = [model.names.get(box.cls.item()) for box in results[0].boxes]
+        # YOLO 모델 초기화
+        model = YOLO('./runs/detect/train/weights/bestfood.pt')
+        results = model.predict(file_path, save=True, imgsz=640, conf=0.3)
 
-    return JSONResponse(content={"labels": output_labels}, status_code=200)
+        # 객체 인식 결과를 리스트로 저장
+        output_labels = [model.names.get(box.cls.item()) for box in results[0].boxes]
+
+        return JSONResponse(content={"labels": output_labels}, status_code=200)
+
+    except Exception as e:
+        # 오류 발생시 예외 처리
+        raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
     import uvicorn
